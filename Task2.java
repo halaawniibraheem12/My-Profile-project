e459188841aaf240458ff8c50096101018b11fbf
halaawniibraheem12 <hala.ibraheemxx@gmail.com>
2025-12-01 12:25:00 +0200

    Add Task2.java to display product list

---
+++ Task2.java
@@ -0,0 +1,15 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        String[] products = {
+            "Tote Bage - 40$",
+            "Sunglassess - 30$",
+            "Wallet - 15$"     ,
+           "Scarf   - 25$"
+        };
+
+        for (String p : products) {
+            System.out.println(p);
+        }
+    }
+}
