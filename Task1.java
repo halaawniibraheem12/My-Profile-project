040aacb0275d60b7dbe25de3423126cf11bce967
halaawniibraheem12 <hala.ibraheemxx@gmail.com>
2025-12-01 12:07:19 +0200

    Implement student information display in Task1.java

---
+++ Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        String name = "Hala Awni Ibraheem";
+        int age = 21;
+        String major = "Computer Engineering";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Major: " + major);
+    }
+}
